83c63eb21c3afa37da873b6d36397cd590e6b04d
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-03-09 14:54:16 +0530

    updated navbar

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
         try {
             await axios.post(`${BaseUrl}/auth/logout`, { withCredentials: true });
             document.cookie = "authToken=; max-age=0; path=/; domain=pizzer-backend.onrender.com;";
-            // window.location.href = "/auth";
+            window.location.href = "/auth";
             window.location.reload();
         } catch (error) {
             console.error("Error signing out:", error.response?.data || error.message);
